722be26f58c251794f2c120665578d7ade31941d
Hemant <hemantshrivastava201@gmail.com>
2019-03-01 18:43:11 +0530

    Template has been created successfully!!!!

--- online-shopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
+++ online-shopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
@@ -1,7 +1,9 @@
 package net.kzn.onlineshopping.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -9,8 +11,32 @@ public class PageController {
 
 	@RequestMapping(value = {"/","/home","/index"})
 	public ModelAndView index() {
-		ModelAndView mv = new ModelAndView("Page");
+		ModelAndView mv = new ModelAndView("home");
 		mv.addObject("greeting", "Welcome to MVC");
+		return mv;
+	}
+	
+//	@RequestMapping(value= {"/test"})
+//	public ModelAndView test(@RequestParam(value = "greeting",required = false) String greeting)
+//	{
+//		if(greeting==null)
+//		{
+//			greeting = "Hello";
+//		}
+//		ModelAndView mv = new ModelAndView("home");
+//		mv.addObject("greeting",greeting);
+//		return mv;
+//	}
+	
+	@RequestMapping(value= {"/test/{greeting}"})
+	public ModelAndView test(@PathVariable("greeting")String greeting)
+	{
+		if(greeting==null)
+		{
+			greeting = "Welcome";
+		}
+		ModelAndView mv = new ModelAndView("home");
+		mv.addObject("greeting", greeting);
 		return mv;
 	}
 }
